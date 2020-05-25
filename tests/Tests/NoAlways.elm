5c9c17f2306142988101ebcd035abbee79e2c637
Phill Sparks <me@phills.me.uk>
2020-05-25 14:16:28 +0100

    Small test refactor

--- tests/Tests/NoAlways.elm
+++ tests/Tests/NoAlways.elm
@@ -8,269 +8,267 @@ import Test exposing (Test, describe, test)
 all : Test
 all =
     describe "NoAlways"
-        [ testLocations
-        , testTypes
+        [ describe "locations" testLocations
+        , describe "types" testTypes
         ]
 
 
-testLocations : Test
+testLocations : List Test
 testLocations =
-    Test.concat
-        [ test "always in brackets does not add extra brackets" <|
-            \_ ->
-                """
+    [ test "always in brackets does not add extra brackets" <|
+        \_ ->
+            """
 module Main exposing (main)
 import Foo exposing (foo)
 main = foo (always "foo")
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Main exposing (main)
 import Foo exposing (foo)
 main = foo (\\_ -> "foo")
 """
-                        ]
-        , test "always in a list" <|
-            \_ ->
-                """
+                    ]
+    , test "always in a list" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo =
     [ "foo", always "bar" ]
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo =
     [ "foo", (\\_ -> "bar") ]
 """
-                        ]
-        , test "always in a map" <|
-            \_ ->
-                """
+                    ]
+    , test "always in a map" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo =
     List.map (always 0) [ 1, 2, 3, 4 ]
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo =
     List.map (\\_ -> 0) [ 1, 2, 3, 4 ]
 """
-                        ]
-        , test "always in a record" <|
-            \_ ->
-                """
+                    ]
+    , test "always in a record" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo =
     { foo = "foo"
     , bar = always "bar"
     }
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo =
     { foo = "foo"
     , bar = (\\_ -> "bar")
     }
 """
-                        ]
-        , test "always in a tuple" <|
-            \_ ->
-                """
+                    ]
+    , test "always in a tuple" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo =
     ( "foo", always "bar" )
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo =
     ( "foo", (\\_ -> "bar") )
 """
-                        ]
-        , test "always pipe right" <|
-            \_ ->
-                """
+                    ]
+    , test "always pipe right" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = "foo" |> always
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectNoErrors
-        , test "always pipe left" <|
-            \_ ->
-                """
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    , test "always pipe left" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always <| heavyComputation "foo"
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectNoErrors
-        ]
+                |> Review.Test.run rule
+                |> Review.Test.expectNoErrors
+    ]
 
 
-testTypes : Test
+testTypes : List Test
 testTypes =
-    Test.concat
-        [ test "always always" <|
-            \_ ->
-                """
+    [ test "always always" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always (always True)
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.atExactly { start = { row = 3, column = 7 }, end = { row = 3, column = 13 } }
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.atExactly { start = { row = 3, column = 7 }, end = { row = 3, column = 13 } }
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> always True)
 """
-                        , alwaysError
-                            |> Review.Test.atExactly { start = { row = 3, column = 15 }, end = { row = 3, column = 21 } }
-                            |> Review.Test.whenFixed
-                                """
+                    , alwaysError
+                        |> Review.Test.atExactly { start = { row = 3, column = 15 }, end = { row = 3, column = 21 } }
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = always (\\_ -> True)
 """
-                        ]
-        , test "always Bool" <|
-            \_ ->
-                """
+                    ]
+    , test "always Bool" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always True
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> True)
 """
-                        ]
-        , test "always Float" <|
-            \_ ->
-                """
+                    ]
+    , test "always Float" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always 4.2
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> 4.2)
 """
-                        ]
-        , test "always Int" <|
-            \_ ->
-                """
+                    ]
+    , test "always Int" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always 42
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> 42)
 """
-                        ]
-        , test "always List" <|
-            \_ ->
-                """
+                    ]
+    , test "always List" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always [1, 2, 3]
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> [1, 2, 3])
 """
-                        ]
-        , test "always Just" <|
-            \_ ->
-                """
+                    ]
+    , test "always Just" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always (Just 42)
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> Just 42)
 """
-                        ]
-        , test "always String" <|
-            \_ ->
-                """
+                    ]
+    , test "always String" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always "foo"
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> "foo")
 """
-                        ]
-        , test "always Tuple" <|
-            \_ ->
-                """
+                    ]
+    , test "always Tuple" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always ( "foo", "bar" )
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> ("foo", "bar"))
 """
-                        ]
-        , test "always Unit" <|
-            \_ ->
-                """
+                    ]
+    , test "always Unit" <|
+        \_ ->
+            """
 module Foo exposing (foo)
 foo = always ()
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ alwaysError
-                            |> Review.Test.whenFixed
-                                """
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ alwaysError
+                        |> Review.Test.whenFixed
+                            """
 module Foo exposing (foo)
 foo = (\\_ -> ())
 """
-                        ]
-        ]
+                    ]
+    ]
 
 
 alwaysError : Review.Test.ExpectedError
